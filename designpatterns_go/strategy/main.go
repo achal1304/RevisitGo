735e1926aff887ee610c9faead80ede7e2e64926
achal1304 <achal20000@gmail.com>
2025-03-27 21:53:12 +0530

    strategy pattern

---
+++ designpatterns_go/strategy/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Strategy interface {
+	Execute(a, b int) int
+}
+
+type Add struct{}
+
+func (Add) Execute(a, b int) int { return a + b }
+
+type Multiply struct{}
+
+func (Multiply) Execute(a, b int) int { return a * b }
+
+func main() {
+	var strategy Strategy = Add{}
+	fmt.Println("Add:", strategy.Execute(2, 3))
+
+	strategy = Multiply{}
+	fmt.Println("Multiply:", strategy.Execute(2, 3))
+}
